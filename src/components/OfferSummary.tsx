89ca60439fd438d394078957a3518b56d4ccbccc
Juliensunlib <dsi@sunlib.fr>
2025-08-03 06:49:14 +0200

    Add default export to OfferSummary component

---
+++ src/components/OfferSummary.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface OfferSummaryProps {
+  // Add props as needed
+}
+
+const OfferSummary: React.FC<OfferSummaryProps> = () => {
+  return (
+    <div>
+      {/* Component content will be added here */}
+    </div>
+  );
+};
+
+export default OfferSummary;
